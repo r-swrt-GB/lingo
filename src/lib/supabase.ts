86e462fab75866ef1441948cb24555e4c1816740
Rikus Swart <rikus.swart@greenbit.dev>
2026-06-25 15:37:39 +0200

    extended auth session

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,8 +3,15 @@ import { createClient } from "@supabase/supabase-js";
 const url = import.meta.env.VITE_SUPABASE_URL as string;
 const key = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
+const isBrowser = typeof window !== "undefined";
+
 export const supabase = createClient(url, key, {
-  ...(typeof window === "undefined" && {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    storage: isBrowser ? localStorage : undefined,
+  },
+  ...(!isBrowser && {
     realtime: { transport: class {} as never },
   }),
 });
